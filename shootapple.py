39e8768f4f914763a5e52de3edfc1c2a46164516
Euhid Aman <aman.derax20@gmail.com>
2020-08-04 20:41:35 +0530

    Added apple image and wrote the game code

---
+++ shootapple.py
@@ -0,0 +1,21 @@
+from random import randint
+
+apple = Actor("apple")
+
+def draw():
+    screen.clear()
+    apple.draw()
+
+def place_apple():
+    apple.x = randint(10, 800)
+    apple.y = randint(10, 600)
+
+def on_mouse_down(pos):
+    if apple.collidepoint(pos):
+        print("good shoot!")
+        place_apple()
+    else:
+        print("You missed!")
+        quit()
+
+place_apple()
